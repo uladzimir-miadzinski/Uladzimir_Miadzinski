929d7d16b7daba95fec3089dc8fcca1d509e5afa
medinsky <webmedinsky@gmail.com>
2018-10-07 23:51:00 +0300

    removed shelljs by copyfiles devDep

--- include-statics.ts
+++
@@ -1,5 +0,0 @@
-import * as shell from 'shelljs';
-
-shell.cp('-R', 'backend/server.crt', 'dist/server.crt');
-shell.cp('-R', 'backend/server.key', 'dist/server.key');
-shell.cp('-R', 'backend/users.json', 'dist/users.json');
