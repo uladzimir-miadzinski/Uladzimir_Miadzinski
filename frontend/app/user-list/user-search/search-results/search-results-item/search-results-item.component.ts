313ef8a90184ce836b1c6d2cf6a165052086bc4d
Uladzimir Miadzinsky <webmedinsky@gmail.com>
2018-10-26 15:22:59 +0300

    choosen click opens list, search div content editable

---
+++ frontend/app/user-list/user-search/search-results/search-results-item/search-results-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-results-item',
+  templateUrl: './search-results-item.component.html',
+  styleUrls: ['./search-results-item.component.scss']
+})
+export class SearchResultsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
