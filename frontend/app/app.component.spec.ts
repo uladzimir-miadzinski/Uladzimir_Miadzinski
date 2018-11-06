ce8011f77af06d5d8b637f8cef086a7003b1ec20
medinsky <webmedinsky@gmail.com>
2018-11-06 22:10:39 +0300

    first working jest test

--- frontend/app/app.component.spec.ts
+++ frontend/app/app.component.spec.ts
@@ -5,14 +5,36 @@ import {
 } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './loading/loading.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatProgressBarModule, MatSelectModule } from '@angular/material';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './http-loader.factory';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-describe('HelloEweComponent', () => {
+describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      imports: [
+        RouterTestingModule,
+        MatSelectModule,
+        MatProgressBarModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ],
+      declarations: [
+        AppComponent,
+        LoadingComponent,
+      ]
     });
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
